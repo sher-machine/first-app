da3c3caa4c1dd11ba6c1998934e71ed6bf9ee768
Egor Shershnev <shershnev1998@mail.ru>
2019-09-14 15:47:27 +0300

    Other button and button for exit

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
@@ -12,30 +16,74 @@ import static android.widget.Toast.LENGTH_LONG;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn;
+    private Button btn, btn2, btn3;         //тип данных для кнопки
     private EditText pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        onButtonClick() ;
+        addListenerOnButton() ;
     }
 
-    public void onButtonClick(){
+    public void addListenerOnButton(){
         pass = (EditText)findViewById(R.id.editText);
         btn = (Button)findViewById(R.id.but1);
-        btn.setOnClickListener(
-                new View.OnClickListener(){
+        btn2= (Button)findViewById(R.id.other_button);
+        btn3= (Button)findViewById(R.id.alert);
+
+        btn.setOnClickListener(                 //обработчик событий
+                new View.OnClickListener(){     //новое действие при нажатии на кнопку
                     @Override
                     public void onClick(View v) {
-                        Toast.makeText(                //Всплывающее окно
+                        btn.setText("DONE");                     //Установка текста после нажатия
+                        btn.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));     //
+                        Toast.makeText(                // метод создания всплывающего окна
                                 MainActivity.this, pass.getText(), LENGTH_LONG
                         ).show();                   // метод, который покажет всплвающее окно
                     }
                 }
         );
+
+        btn2.setOnClickListener(                 //обработчик событий
+                new View.OnClickListener(){     //новое действие при нажатии на кнопку
+                    @Override
+                    public void onClick(View v) {
+                        btn.setBackgroundTintList(ColorStateList.valueOf(Color.RED));     //
+                        Toast.makeText(                     // метод создания всплывающего окна
+                                MainActivity.this, "Other_button", LENGTH_LONG
+                        ).show();                            // метод, который покажет всплвающее окно
+                    }
+                }
+        );
+
+
+
+        btn3.setOnClickListener(                 //обработчик событий
+                new View.OnClickListener() {     //новое действие при нажатии на кнопку
+                    @Override
+                    public void onClick(View v) {
+                        AlertDialog.Builder a_builder = new AlertDialog.Builder(MainActivity.this); //билдер всплывающих окон
+                        a_builder.setMessage("Do u want close app?")
+                                .setCancelable(false)   //нет кнопки закрыть
+                                .setPositiveButton("YES", new DialogInterface.OnClickListener() {   //обработчик нажатия на кнопку "YES"
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();   //полное закрытие прилолжения
+                                    }
+                                })
+                                .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel(); //закрыть всплывающее окно
+                                    }
+                                });
+                        AlertDialog alert = a_builder.create();         //само всплывающее окно
+                        alert.setTitle("Close app");         //Заголовок для всплывающего окна
+                        alert.show();
+                    }
+                }
+        );
     }
-
-
 }
+
